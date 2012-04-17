34f4111ca080aabcf26b3749ea88616b426dbe14
patrick <if10b047@technikum-wien.at>
2012-04-17 05:02:20 +0200

    Initial Commit

---
+++ SWPUE4-Composite/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SWPUE4-Composite
+//
+//  Created by Patrick Schwab on 4/17/12.
+//  Copyright (c) 2012 Patrick Schwab. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
